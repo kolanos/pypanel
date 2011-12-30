316f959fbc63ec91e58aa96004279d9a1ba9c60d
Michael Lavers <kolanos@gmail.com>
2011-12-29 19:50:33 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='pypanel',
+    version='0.1',
+    packages=['pypanel'],
+    author='Michael Lavers',
+    author_email='kolanos@gmail.com',
+    url='http://www.pypanel.org/',
+    description='PyPanel is a web hosting control panel written entirely in Python.',	 
+    platforms=[],
+    license='MIT',
+    scripts=[]
+)
