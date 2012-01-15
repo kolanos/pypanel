cdc3c75607b50189eb1e741617baa300daef6a3c
Michael Lavers <kolanos@gmail.com>
2012-01-14 22:41:51 -0800

    Add operating system check to install script

--- scripts/install.py
+++ scripts/install.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
+from __future__ import with_statement
 from fabric.api import *
-from fabric.colors import white, red
+from fabric.colors import green, red, white, yellow
 from fabric.network import prompt_for_password
 
 def main():
     """Installation main menu"""
     local('clear')
-    print "Welcome to the %s installation!\n" % white("PyPanel", True)
-    print "%s Ensure that your system is a clean install of %s before proceeding.\n" % (red("IMPORTANT:", True),
-                                                                                        white("Ubuntu Server Edition 10.04", True))
-    print white("Select from the options below:\n", True)
-    print "1. Install PyPanel now"
-    print "2. Exit installation\n"
+    print("Welcome to the %s installation!\n" % white("PyPanel", True))
+    print("%s Ensure that your system is a clean install of %s before proceeding.\n" % (yellow("IMPORTANT:", True),
+                                                                                        white("Ubuntu Server Edition 10.04", True)))
+    print(white("Select from the options below:\n", True))
+    print("1. Install PyPanel now")
+    print("2. Exit installation\n")
 
     option = prompt("Enter an option:", default="2", validate=r"^[1-2]$")
 
@@ -20,24 +21,30 @@ def main():
         local('clear')
         install()
     elif option == '2':
-        print "\nExiting..."
+        print("\nExiting...")
         exit()
 
 def install():
     """Begin installation"""
-    print white("\nBegin installation!\n", True)
-    print "Before we begin, we need to know where you want to install PyPanel."
-    print "You can either install remotely or locally."
-    print "Either way we require root access in order to install."
-    print "So if you don't have your root password, get it now."
-    print "Also make sure that you can SSH into the remote/local host as root.\b"
+    print(white("\nBegin installation!\n", True))
+    print("Before we begin, we need to know where you want to install PyPanel.")
+    print("You can either install remotely or locally.")
+    print("Either way we require root access in order to install.")
+    print("So if you don't have your root password, get it now.")
+    print("Also make sure that you can SSH into the remote/local host as root.\n")
 
-    host = prompt("Enter server hostname or IP:", default="localhost")
-    user = prompt("Enter root username:", default="root")
-    password = prompt_for_password("Enter root password: ", True)
+    env.host = prompt("Enter server hostname or IP:", default="localhost")
+    env.host_string = env.host
+    env.user = prompt("Enter root username:", default="root")
+    env.password = prompt_for_password("Enter root password: ", True)
 
-    env.user = user
-    env.hosts = [host]
+    with hide('running', 'stdout'):
+        server_version = run('cat /etc/issue')
+    if 'Ubuntu 10.04' in server_versio:
+        print("\n%s Server is running Ubuntu 10.04" % green("[SUCCESS]", True))
+    else:
+        print("\n%s Server is not running Ubuntu 10.04" % red("[FAILURE]", True))
+        exit()
 
 def ntpdate():
     """Create daily ntpdate cron job, run ntpdate for good measure."""
